4d3c013eb62d58a584ec0413216b8890a8f5ad6a
MagnusRasmussen <mlra10@student.aau.dk>
2014-04-24 13:59:58 +0200

    made a master project folder, and copied code from BlackJackConsole to said folder
    master folder is a go

---
+++ Blackjack_Collected/Blackjack_Collected/Deck.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Blackjack_Collected
+{
+	public class Deck
+	{
+		public List<Card> cards = new List<Card>(52);
+
+		public Deck ()
+		{
+			this.SetDeck ();
+		
+		}
+
+		public void SetDeck()
+		{
+			this.cards.Clear ();
+			this.cards.AddRange(Enumerable.Range(1,4).SelectMany(s => Enumerable.Range(1,13).Select(n => new Card((Rank)n, (Suite)s))));
+
+		}
+
+
+		public void Shuffle()
+		{
+			cards = cards.OrderBy(c => Guid.NewGuid()).ToList();     
+		
+		}
+
+		public void Deal(Hand hand)
+		{
+			var card = this.cards.FirstOrDefault();
+			hand.AddCard (card);
+		
+			this.cards.Remove(card);
+
+		}
+
+		public void DrawCard(Hand hand)
+		{
+
+			var card = this.cards.FirstOrDefault();
+			hand.AddCard(this.cards.First());
+            this.cards.Remove(card);
+            		
+		}
+
+        public void DispalyDeck()
+        {
+            {
+                int i;
+
+                for (i = 0; i < cards.Count; i++)
+                {
+
+                    var card = cards[i];
+                    Console.WriteLine(card.Suite + " " + card.Rank);
+
+                }
+                Console.ReadLine();
+            }
+
+
+        }
+
+
+
+	}
+}
+
---
+++ Blackjack_Collected/Blackjack_Collected/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Blackjack_Collected
+{
+	class MainClass
+	{
+		//private Deck deck; it's not being used
+
+		static void Main()
+		{
+			Deck deck = new Deck();
+			deck.SetDeck();
+			deck.Shuffle();
+			deck.DispalyDeck();
+
+		}
+	}
+}
