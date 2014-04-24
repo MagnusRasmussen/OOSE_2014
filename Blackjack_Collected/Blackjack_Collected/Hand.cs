0d1d531ad827a328775e6a4fdf536fe57abaf36f
NicklasJakobsen <ndss10@student.aau.dk>
2014-04-24 15:31:52 +0200

    Updated hand
    Updated hand

--- Blackjack_Collected/Blackjack_Collected/Hand.cs
+++ Blackjack_Collected/Blackjack_Collected/Hand.cs
@@ -9,16 +9,57 @@ namespace Blackjack_Collected
 
 		private List<Card> cards = new List<Card>(5);
 
+
+
 		public Hand ()
 		{
 
 		}
+
+		public int HandValue()
+		{
+
+			int i;
+			int handValue;
+			for (i = 0; i < cards.Count; i++) {
+
+				var card = cards [i];
+				if (card.Rank == 0 && (handValue + 11) < 22) {
+					handValue = 11;
+				} 
+				else {
+					handValue = 1;
+				}
+
+				if (card.Rank > 9) {
+					handValue = 10;
+				} 
+				else {
+					handValue = (int)card.Rank + 1;
+				}
+				handValue += HandValue;
+
+			}
+
+			if (cards.Count < 5) {
+
+			} 
+			else {
+				//PLAYER WON!!
+			}
+
+
+		}
+
+
+
+
 		public void AddCard(Card card)
 		{
 			this.cards.Add(card);
 
 
 		}
+
 	}
 }
-
