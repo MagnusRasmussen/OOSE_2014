de2541308b43234ab7b8777828edb90b2f0e9ddd
Kevin Dahlstrøm <kdahls10@student.aau.dk>
2014-04-24 13:43:17 +0200

    Removed outcommented code and updated Class diagram
    DisplayDeck was added to Class diagram

--- BlackJackConsole/BlackJackConsole/Deck.cs
+++ BlackJackConsole/BlackJackConsole/Deck.cs
@@ -18,33 +18,6 @@ namespace BlackJackConsole
 		{
 			this.cards.Clear ();
 			this.cards.AddRange(Enumerable.Range(1,4).SelectMany(s => Enumerable.Range(1,13).Select(n => new Card((Rank)n, (Suite)s))));
-//			Console.WriteLine (cards.SingleOrDefault);
-//
-////			cards.Sum
-//			cards.ForEach (Console.WriteLine);
-//			Console.WriteLine("BATMAN!!");
-		
-            //var card = this.cards.FirstOrDefault();
-            //Console.WriteLine(card.Rank + " " + card.Suite);
-            //Console.ReadLine ();
-            
-            //foreach (var tal in cards) {
-            //    Console.WriteLine(cards.card.Rank + " " + card.Suite);
-            //    Console.ReadLine ();
-            //}
-
-            //int i;
-            //for (i = 0; i < 52; i++)
-            //{
-            //    //for (j = 1; j < 2; j++)
-            //    //{
-            //    var card = cards[i];
-            //    Console.WriteLine(card.Suite + " " + card.Rank);
-            //    //Console.ReadLine();
-            //    //}
-            //    //Console.WriteLine();
-            //}
-            //Console.ReadLine();
 
 		}
 
@@ -52,11 +25,7 @@ namespace BlackJackConsole
 		public void Shuffle()
 		{
 			cards = cards.OrderBy(c => Guid.NewGuid()).ToList();     
-			
-
-            //var card = this.cards.FirstOrDefault();
-            //Console.WriteLine(card.Rank + "" + card.Rank);
-            //Console.ReadLine();
+		
 		}
 
 		public void Deal(Hand hand)
@@ -66,7 +35,6 @@ namespace BlackJackConsole
 		
 			this.cards.Remove(card);
 
-//			card = this.cards.First();
 		}
 
 		public void DrawCard(Hand hand)
--- BlackJackConsole/BlackJackConsole/Program.cs
+++ BlackJackConsole/BlackJackConsole/Program.cs
@@ -3,54 +3,22 @@
 
 namespace BlackJackConsole
 {
-	class MainClass
-	{
-		private Deck deck;
-	
-		 
-		 static void Main ()
-		{
+    class MainClass
+    {
+        private Deck deck;
+
+
+        static void Main()
+        {
             Deck deck = new Deck();
             deck.SetDeck();
             deck.Shuffle();
             deck.DispalyDeck();
 
+        }
+    }
 
-		}
 
-//		public void ShowDeck()
-//		{
-//			deck.SetDeck ();
-//		}
+}
 
-		public void WriteToConsole()
-		{
-
-		}
-
-		public void DisplayDeck ()
-		{
-            int i;
-            //int j;
-
-            for (i = 1; i < 52; i++)
-            {
-                //for (j = 1; j < 2; j++)
-                //{
-                    var card = deck.cards[i];
-                    Console.WriteLine(card.Suite + " " + card.Rank);
-                    Console.ReadLine();
-                //}
-                //Console.WriteLine();
-            }
-            //foreach (var tal in deck.cards) {
-            //    Console.WriteLine (deck.cards);
-            //    Console.ReadLine ();
-			}
-        
-		}
-
-	}
-
-//}
 
